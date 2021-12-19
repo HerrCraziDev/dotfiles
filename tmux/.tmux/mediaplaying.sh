3464039d98deed4d30fb0ea54d3d83825edca219
HerrCraziDev <herrcrazi@gmail.com>
2021-12-19 03:55:15 +0100

    Small changes, tmux shortcuts

--- tmux/.tmux/mediaplaying.sh
+++ tmux/.tmux/mediaplaying.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if ! command -v playerctl; then
+if ! hash playerctl 2>/dev/null; then
 	return 1
 fi
 
 playing=$(playerctl -a metadata --format='{{status}}:{{artist}} - {{title}}' | grep 'Playing' | sed -e "s/^Playing://")
+#echo $playing
 
 if [ -n "$playing" ]; then
 	echo $playing | sed -E 's/(.{30})(.{1,})$/\1.../'
